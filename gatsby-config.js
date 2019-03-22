85ffe2c3ee5b632b483e018f796c1fd3cd5ef66e
bkleeman <benjamin.kleeman@gmail.com>
2019-03-21 23:22:22 -0500

    updated manifest w/ appropriate name & standalone display

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'benjamin-kleeman-personal-website',
+        short_name: 'benjamin-kleeman',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
-        display: 'minimal-ui',
+        display: 'standalone',
         icon: 'src/images/icon-512x512.png', // This path is relative to the root of the site.
       },
     },
